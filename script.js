1739d52ae830c9052956902774362c971d51188f
Sergio Gomez <gomez.sergio14@gmail.com>
2022-12-08 15:00:25 -0300

    calculo de timepo agregado

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 const cuadro = document.querySelectorAll(".cuadro");
-const btnIniciar = document.getElementById("btnIniciar");
 const btnMostrar =document.getElementById("mostrarJugadores");
 const contenedorJugadores=document.getElementById("contenedorJugadores");
-// const nombreJugador = document.getElementById("nombreJugador");
-// const ul = document.getElementById("listaJugadores");
+let inicioTiempo;
+let finTiempo;
+let tiempoUtilizado;
 let controlm = false;
 let auxm = false;
 let cuadroUno, cuadroDos;
-// let jugadores = [];
-// let dificultadElegida = "dificil";
 let contador=0;
-
-// class Jugador {
-//   constructor(nombre, dificultad) {//acordate de sacar lo de la dificultad
-//     this.nombre = nombre;
-//     this.dificultad = dificultad;
-//   }
-// }
-
-
 //FUNCIONES
 
 function entrar(){
+  inicioTiempo=new Date();
+  inicioTiempo=Number(inicioTiempo.getTime());
+  let segundaPantalla=document.getElementById("2p");
   let contenedor0=document.getElementById("c0");
   let contenedor1=document.getElementById("c1");
+  segundaPantalla.className="muestro2p";
   contenedor1.className ="muestro";
   contenedor0.className ="oculto";
+  
 }
 
 function darVuelta() {
@@ -44,9 +38,19 @@ function darVuelta() {
 function coincidencia() {
   let sonIguales = cuadroUno.dataset.carta === cuadroDos.dataset.carta;
   sonIguales ? desactivarCuadros() : ocultar();
-  // if(contador==8){
-  //   alert("todos los pares encontrados");
-  // }
+}
+
+function fin(){
+  finTiempo=new Date();
+  finTiempo=Number(finTiempo.getTime());
+  // tiempoUtilizado=finTiempo-inicioTiempo;
+  tiempoUtilizado = Math.floor(.5+(finTiempo-inicioTiempo)/1000);
+  Swal.fire({
+    icon: 'success',
+    title: 'Todos los pares encontrados',
+    showConfirmButton: false,
+    timer: 2500
+  })
 }
 
 function desactivarCuadros() {
@@ -58,15 +62,7 @@ function desactivarCuadros() {
     setTimeout(fin,210);
   }
 }
-function fin(){
-  // alert("todos los pares encontrados");
-  Swal.fire({
-    icon: 'success',
-    title: 'Todos los pares encontrados',
-    showConfirmButton: false,
-    timer: 2500
-  })
-}
+
 
 function ocultar() {
   auxm = true;
@@ -88,44 +84,35 @@ function reset() {
     cuadro.style.order = randomPos;
   });
 })();
+
 async function traerPokemon(id){
   const response= await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
   const data= await response.json()
-  //console.log(data);
   crearTarjeta(data);
 }
+
 function generarJugadores(cantidad){
   for(let i=1;i<=cantidad;i++){
     let j= Math.ceil(Math.random()*200);
     traerPokemon(j);
   }
 }
+
 function crearTarjeta(pokemon){
-  //contenedorJugadores.innerHTML=``;
   const contenedorCarta=document.createElement("div");
   const tarjeta=`<div class="card" style="width: 18rem;">
   <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${pokemon.name}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <button class="btn btn-primary" id="btnIniciar" type="submit">INICIAR</button>
+    <button class="btn btn-primary" id="btnIniciar" type="submit" onclick="entrar()">INICIAR</button>
   </div>
 </div>`;
 contenedorCarta.classList.add("col");
 contenedorCarta.innerHTML+=tarjeta;
 contenedorJugadores.appendChild(contenedorCarta);
+
 }
-//codigo de la tarjeta de bootstrap
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">INICIAR</a>
-  </div>
-</div> */}
-
-
 
 //FIN FUNCIONES
 btnMostrar.addEventListener('click',()=>{
@@ -133,13 +120,12 @@ btnMostrar.addEventListener('click',()=>{
   btnMostrar.classList.add("oculto");
 });
 
-
 cuadro.forEach((cuadro) => cuadro.addEventListener("click", darVuelta));
 
 
-btnIniciar.addEventListener("click", (e) => {
-     e.preventDefault();
-    });
+// btnIniciar.addEventListener("click", (e) => {
+//      e.preventDefault();
+//     });
 //comento lo siguiente para empezar a aplicar fetch
 // btnAgregar.addEventListener("click", (e) => {
 //   e.preventDefault();
